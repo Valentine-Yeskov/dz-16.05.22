440d4c950783f42b0b2e404a8cce0d96931dc47c
Valentine-Yeskov <valentineyescov@gmail.com>
2023-05-17 11:32:20 +0300

    all strings

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+       class Employee{
+           String name;
+           String surname;
+           String patronymic;
+           String position;
+           String email;
+           int age;
+           int phoneNumber;
+           float salary;
+
+
+       }
+    }
+}
